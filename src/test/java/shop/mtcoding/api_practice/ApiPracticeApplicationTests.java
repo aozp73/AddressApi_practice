cb800bac73f8bbf8f93b6f379853b10751d5e04a
aozp73 <aozp73@naver.com>
2023-02-15 16:42:23 +0900

    initial commit

---
+++ src/test/java/shop/mtcoding/api_practice/ApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.api_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
