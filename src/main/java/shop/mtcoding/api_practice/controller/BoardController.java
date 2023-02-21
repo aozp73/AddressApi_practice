8e63168f943a83dafa28b4bd95bbd1eacaded25e
aozp73 <aozp73@naver.com>
2023-02-21 18:26:07 +0900

    API 이메일 사용 연습 완료

--- src/main/java/shop/mtcoding/api_practice/controller/BoardController.java
+++ src/main/java/shop/mtcoding/api_practice/controller/BoardController.java
@@ -1,13 +1,8 @@
 package shop.mtcoding.api_practice.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("tes")
-    public String tes() {
-        return "board/tes";
-    }
 }
--- src/main/java/shop/mtcoding/api_practice/controller/UserController.java
+++ src/main/java/shop/mtcoding/api_practice/controller/UserController.java
@@ -1,5 +1,14 @@
 package shop.mtcoding.api_practice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/joinForm")
+    public String loginForm() {
+        return "user/joinForm";
+    }
 
 }
