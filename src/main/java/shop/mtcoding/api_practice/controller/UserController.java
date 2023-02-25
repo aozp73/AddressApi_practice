0e161dca83b85a3cff33041b64d9466d630d63d4
aozp73 <aozp73@naver.com>
2023-02-25 19:44:41 +0900

    API 이메일 사용 연습 완료2

--- src/main/java/shop/mtcoding/api_practice/controller/UserController.java
+++ src/main/java/shop/mtcoding/api_practice/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
+    // 테스트 2
     @GetMapping("/joinForm")
     public String loginForm() {
         return "user/joinForm";
